8b521df40284a8c70948b8a943a45e18c239a548
Huangyb <873825232@qq.com>
2017-11-23 18:04:42 +0800

    提供全新RecyclerView日历，功能更加强大。

--- app/src/main/java/com/palmwifi/testcal/HomeActivity.java
+++ app/src/main/java/com/palmwifi/testcal/HomeActivity.java
@@ -18,8 +18,11 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ac_home);
+        findViewById(R.id.btn_new_month_cal).setOnClickListener(this);
+        findViewById(R.id.btn_new_week_cal).setOnClickListener(this);
         findViewById(R.id.btn_cal).setOnClickListener(this);
         findViewById(R.id.btn_round_board).setOnClickListener(this);
+        findViewById(R.id.btn_dash_board).setOnClickListener(this);
         findViewById(R.id.btn_dash_board).setOnClickListener(this);
     }
 
@@ -27,6 +30,12 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         Intent i = null;
         switch (v.getId()){
+            case R.id.btn_new_month_cal:
+                i = new Intent(this,CalendarMonthActivity.class); //日历
+                break;
+            case R.id.btn_new_week_cal:
+                i = new Intent(this,CalendarWeekActivity.class); //日历
+                break;
             case R.id.btn_cal:
                 i = new Intent(this,CalendarActivity.class); //日历
                 break;
@@ -36,6 +45,7 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_round_board:
                 i = new Intent(this,RingActivity.class); //圆盘
                 break;
+
         }
         if(i  != null){
             startActivity(i);
